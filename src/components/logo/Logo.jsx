91512c9c6f0f080f931e6eccbeda66eb6254a3ad
Sum-Monika2004 <13monika04@gmail.com>
2026-04-11 12:48:10 +0600

    search functionality

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from "../../assets/logo.png";
+import { Link } from "react-router";
 const Logo = () => {
   return (
-    <div className="flex items-center justify-between">
-      <img className="w-10 h-10" src={logo} alt="" />{" "}
-      <h3 className="text-3xl font-bold">SwiftDrop</h3>
-    </div>
+    <Link to="/">
+      <div className="flex items-center justify-between">
+        <img className="w-10 h-10" src={logo} alt="" />{" "}
+        <h3 className="text-3xl font-bold">SwiftDrop</h3>
+      </div>
+    </Link>
   );
 };
 
--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useLoaderData } from "react-router";
@@ -6,15 +6,33 @@ import { useLoaderData } from "react-router";
 const Coverage = () => {
   const position = [23.685, 90.3563];
   const serviceCenters = useLoaderData();
-  console.log(serviceCenters);
+  // console.log(serviceCenters);
+  const mapRef = useRef(null);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const location = e.target.location.value;
+    const district = serviceCenters.find((c) =>
+      c.district.toLowerCase().includes(location.toLowerCase()),
+    );
+
+    if (district) {
+      const coord = [district.latitude, district.longitude];
+      console.log(district, coord);
+      // fly to the location
+      mapRef.current.flyTo(coord, 11);
+    }
+  };
 
   return (
     <div className="w-10/12 mx-auto my-20">
       <h2 className="text-5xl text-primary text-center font-semibold mb-10">
         We are available in 64 districts
       </h2>
+
+      {/* form  */}
       <div className=" mb-10">
-        <form>
+        <form onSubmit={handleSearch}>
           <label className="input">
             <svg
               className="h-[1em] opacity-50"
@@ -32,32 +50,46 @@ const Coverage = () => {
                 <path d="m21 21-4.3-4.3"></path>
               </g>
             </svg>
-            <input type="search" required placeholder="Search" />
+            <input
+              type="search"
+              required
+              name="location"
+              placeholder="Search"
+            />
           </label>
         </form>
       </div>
 
       {/* map container */}
-      <div className="w-full h-150 border">
-        <MapContainer
-          center={position}
-          zoom={8}
-          scrollWheelZoom={false}
-          className="h-150"
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          {serviceCenters.map((center, index) => (
-            <Marker position={[center.latitude, center.longitude]} key={index}>
-              <Popup>
-                <strong>{center.district}</strong> <br />
-                Service Area : {center.covered_area.join(", ")}{" "}
-              </Popup>
-            </Marker>
-          ))}
-        </MapContainer>
+      <div>
+        <h1 className="text-4xl mt-8 mb-5 font-bold ">
+          We Deliver Almost All Over Bangladesh
+        </h1>
+        <div className="w-full h-150 border">
+          <MapContainer
+            center={position}
+            zoom={8}
+            scrollWheelZoom={false}
+            className="h-150"
+            ref={mapRef}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {serviceCenters.map((center, index) => (
+              <Marker
+                position={[center.latitude, center.longitude]}
+                key={index}
+              >
+                <Popup>
+                  <strong>{center.district}</strong> <br />
+                  Service Area : {center.covered_area.join(", ")}{" "}
+                </Popup>
+              </Marker>
+            ))}
+          </MapContainer>
+        </div>
       </div>
     </div>
   );

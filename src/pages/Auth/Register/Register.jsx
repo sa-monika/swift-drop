bcbc47363f6b0c3fd591d93c62119ea0409ac8bb
Sum-Monika2004 <13monika04@gmail.com>
2026-04-14 07:26:24 +0600

    use react hook form in register page and setup firebase

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,18 +1,58 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleRegistration = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className="">
-      <form>
+    <div className="max-w-11/12 mx-auto">
+      <form onSubmit={handleSubmit(handleRegistration)}>
         <fieldset className="fieldset">
+          {/* email  */}
           <label className="label">Email</label>
-          <input type="email" className="input w-full" placeholder="Email" />
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="input w-full"
+            placeholder="Email"
+          />
+          {errors.email?.type === "required" && (
+            <p className="text-red-500 text-sm ">Email is required.</p>
+          )}
+          {/* password  */}
           <label className="label">Password</label>
           <input
             type="password"
+            {...register("password", {
+              required: true,
+              minLength: 6,
+              pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+            })}
             className="input w-full"
             placeholder="Password"
           />
+          {errors.password?.type === "required" && (
+            <p className="text-red-500 text-sm ">Password is required.</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-500 text-sm ">
+              Password must be 6 character or longer.
+            </p>
+          )}
+          {errors.password?.type === "pattern" && (
+            <p className="text-red-500 text-sm ">
+              Password must have at least one uppercase, one lowercase and one
+              number.
+            </p>
+          )}
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>

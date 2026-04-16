720167fd78b8320ed0f99702521d911d1cbadd40
Sum-Monika2004 <13monika04@gmail.com>
2026-04-16 18:43:58 +0600

    private route setup

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -91,6 +91,7 @@ const Navbar = () => {
             </Link>
           </div>
         )}
+        {/* <Link className="btn btn-primary">Be a Rider</Link> */}
       </div>
     </div>
   );
---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import useAuth from "../Hooks/UseAuth";
+import { Navigate } from "react-router";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <PropagateLoader color="#ffaf13" />;
+  }
+  if (!user) {
+    return <Navigate to="/login"></Navigate>;
+  }
+  return children;
+};
+
+export default PrivateRoute;

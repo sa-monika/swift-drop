2bbff2b0ced9a3e6a891d446ceff02aa81b45406
Sum-Monika2004 <13monika04@gmail.com>
2026-04-21 23:28:51 +0600

    radio button in send parcel form

--- src/pages/SendParcel/SendParcel.jsx
+++ src/pages/SendParcel/SendParcel.jsx
@@ -8,27 +8,59 @@ const SendParcel = () => {
     formState: { errors },
   } = useForm();
 
-  const handleSendParcel = (data) => {};
+  const handleSendParcel = (data) => {
+    console.log(data);
+  };
   return (
-    <div>
+    <div className="max-w-11/12 mx-auto">
       <h2 className="text-5xl font-semibold text-center text-primary my-10">
         Send Parcel
       </h2>
 
-      <form onSubmit={handleSubmit(handleSendParcel)}>
-        {/* document  */}
-        <div></div>
+      <form className="mt-12 p-4" onSubmit={handleSubmit(handleSendParcel)}>
+        {/* parcel type  */}
+        <div>
+          <label className="label mr-4">
+            <input
+              type="radio"
+              {...register("parcelType")}
+              className="radio radio-primary"
+              defaultChecked
+              value="document"
+            />
+            Document
+          </label>
+          <label className="label">
+            <input
+              type="radio"
+              {...register("parcelType")}
+              className="radio radio-primary"
+              defaultChecked
+              value="non-document"
+            />
+            Non-document
+          </label>
+        </div>
 
         {/* parcel info : name ,weight  */}
         <div></div>
         {/* two column  */}
-        <div>
+        <div className="flex justify-between items-center">
           {/* sender info  */}
-          <div></div>
+          <div>
+            <h2>Sender Details</h2>
+          </div>
 
           {/* receiver info  */}
-          <div></div>
+          <div>
+            <h2>Receiver Details</h2>
+          </div>
         </div>
+        <input
+          type="submit"
+          value="Send Parcel"
+          className="btn btn-primary text-black"
+        />
       </form>
     </div>
   );

fff766e9718df76e0384a565f18c2fcfda66177a
Sum-Monika2004 <13monika04@gmail.com>
2026-04-22 22:00:01 +0600

    form section of send parcel

--- src/pages/SendParcel/SendParcel.jsx
+++ src/pages/SendParcel/SendParcel.jsx
@@ -17,7 +17,10 @@ const SendParcel = () => {
         Send Parcel
       </h2>
 
-      <form className="mt-12 p-4" onSubmit={handleSubmit(handleSendParcel)}>
+      <form
+        className="mt-12 p-4 space-y-8"
+        onSubmit={handleSubmit(handleSendParcel)}
+      >
         {/* parcel type  */}
         <div>
           <label className="label mr-4">
@@ -43,17 +46,168 @@ const SendParcel = () => {
         </div>
 
         {/* parcel info : name ,weight  */}
-        <div></div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 ">
+          {/*parcel name  */}
+          <fieldset>
+            <label className="label font-bold text-[16px]">Parcel Name</label>
+            <input
+              type="text"
+              {...register("parcelName", { required: true })}
+              className="input w-full"
+              placeholder="Parcel Name"
+            />
+          </fieldset>
+          {/*parcel weight   */}
+          <fieldset>
+            <label className="label font-bold text-[16px]">Parcel Weight</label>
+            <input
+              type="number"
+              {...register("parcelWeight", { required: true })}
+              className="input w-full"
+              placeholder="Parcel Weight"
+            />
+          </fieldset>
+        </div>
+
         {/* two column  */}
-        <div className="flex justify-between items-center">
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
           {/* sender info  */}
           <div>
-            <h2>Sender Details</h2>
+            <fieldset>
+              <h2 className="text-2xl font-bold text-secondary mb-4">
+                Sender Details
+              </h2>
+              {/* sender name  */}
+              <label className="label font-bold text-[16px]">Sender Name</label>
+              <input
+                type="text"
+                {...register("senderName", { required: true })}
+                className="input w-full"
+                placeholder="Sender Name"
+              />
+
+              {/* sender address  */}
+              <label className="label font-bold text-[16px] mt-4">
+                Sender Address
+              </label>
+              <input
+                type="text"
+                {...register("senderAddress", { required: true })}
+                className="input w-full"
+                placeholder="Sender Address"
+              />
+              {/* sender district  */}
+              <label className="label font-bold text-[16px] mt-4">
+                Sender District
+              </label>
+              <input
+                type="text"
+                {...register("senderDistrict", { required: true })}
+                className="input w-full"
+                placeholder="Sender District"
+              />
+              {/* sender phone no.  */}
+              <label className="label font-bold text-[16px] mt-4">
+                Sender Phone No.
+              </label>
+              <input
+                type="number"
+                {...register("senderPhoneNo", { required: true })}
+                className="input w-full"
+                placeholder="Sender Phone No."
+              />
+              {/* Your district  */}
+              <label className="label font-bold text-[16px] mt-4">
+                Your District
+              </label>
+
+              <input
+                type="text"
+                {...register("yourDistrict", { required: true })}
+                className="input w-full"
+                placeholder="Your District"
+              />
+              {/* pickup instructions  */}
+              <label className="label font-bold text-[16px] mt-4">
+                Pickup Instructions
+              </label>
+              <textarea
+                {...register("pickupInstructions", { required: true })}
+                className="textarea w-full"
+                placeholder="Pickup Instructions"
+              ></textarea>
+            </fieldset>
           </div>
 
           {/* receiver info  */}
           <div>
-            <h2>Receiver Details</h2>
+            <fieldset>
+              <h2 className="text-2xl font-bold text-secondary mb-4">
+                Receiver Details
+              </h2>
+              {/* Receiver name  */}
+              <label className="label font-bold text-[16px]">
+                Receiver Name
+              </label>
+              <input
+                type="text"
+                {...register("receiverName", { required: true })}
+                className="input w-full"
+                placeholder="Receiver Name"
+              />
+
+              {/* Receiver address  */}
+              <label className="label font-bold text-[16px] mt-4">
+                Receiver Address
+              </label>
+              <input
+                type="text"
+                {...register("receiverAddress", { required: true })}
+                className="input w-full"
+                placeholder="Receiver Address"
+              />
+              {/* Receiver district  */}
+              <label className="label font-bold text-[16px] mt-4">
+                Receiver District
+              </label>
+              <input
+                type="text"
+                {...register("receiverDistrict", { required: true })}
+                className="input w-full"
+                placeholder="Receiver District"
+              />
+              {/* Receiver phone no.  */}
+              <label className="label font-bold text-[16px] mt-4">
+                Receiver Phone No.
+              </label>
+              <input
+                type="number"
+                {...register("receiverPhoneNo", { required: true })}
+                className="input w-full"
+                placeholder="Receiver Phone No."
+              />
+              {/* Your district  */}
+              <label className="label font-bold text-[16px] mt-4">
+                Your District
+              </label>
+
+              <input
+                type="text"
+                {...register("yourDistrict", { required: true })}
+                className="input w-full"
+                placeholder="Your District"
+              />
+              {/* pickup instructions  */}
+              <label className="label font-bold text-[16px] mt-4">
+                Pickup Instructions
+              </label>
+              <textarea
+                {...register("pickupInstructions", { required: true })}
+                className="textarea w-full"
+                placeholder="Pickup Instructions"
+              ></textarea>
+            </fieldset>
           </div>
         </div>
         <input

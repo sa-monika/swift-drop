5f4ec0185c8f8905a5cf3df5c1003453770c350c
Sum-Monika2004 <13monika04@gmail.com>
2026-05-01 23:37:27 +0600

    get parcels by email

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -1,7 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import useAuth from "../../../Hooks/UseAuth";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const MyParcels = () => {
-  return <div></div>;
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: parcels = [] } = useQuery({
+    queryKey: ["myParcels", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/parcels?email=${user.email}`);
+
+      return res.data;
+    },
+  });
+  return (
+    <div>
+      <h2>My Parcels: {parcels.length}</h2>
+    </div>
+  );
 };
 
 export default MyParcels;

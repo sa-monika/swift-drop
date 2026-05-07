0d0e47d8362f74dd8f20d7c937d933d58c060fd9
Sum-Monika2004 <13monika04@gmail.com>
2026-05-08 00:13:09 +0600

    delete parcel

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -5,11 +5,12 @@ import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { FiEdit } from "react-icons/fi";
 import { HiMiniMagnifyingGlass } from "react-icons/hi2";
 import { AiFillDelete } from "react-icons/ai";
+import Swal from "sweetalert2";
 
 const MyParcels = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
-  const { data: parcels = [] } = useQuery({
+  const { data: parcels = [], refetch } = useQuery({
     queryKey: ["myParcels", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/parcels?email=${user.email}`);
@@ -17,6 +18,36 @@ const MyParcels = () => {
       return res.data;
     },
   });
+
+  const handleParcelDelete = (id) => {
+    console.log(id);
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/parcels/${id}`).then((res) => {
+          console.log(res.data);
+          if (res.data.deletedCount) {
+            // refresh the data in the ui
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your parcel request has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+
   return (
     <div>
       <h2 className="text-primary text-4xl  font-semibold text-center my-4">
@@ -50,7 +81,10 @@ const MyParcels = () => {
                   <button className="btn btn-square hover:bg-primary">
                     <HiMiniMagnifyingGlass />
                   </button>
-                  <button className="btn btn-square hover:bg-primary">
+                  <button
+                    onClick={() => handleParcelDelete(parcel._id)}
+                    className="btn btn-square hover:bg-primary"
+                  >
                     <AiFillDelete />
                   </button>
                 </td>

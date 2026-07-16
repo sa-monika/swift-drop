c85034931b14a55232c3d46a83a1e23111dace14
Sum-Monika2004 <13monika04@gmail.com>
2026-07-16 13:16:14 +0600

    admin dashbaord

--- src/pages/Dashboard/DashboardHome/AdminDashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome/AdminDashboardHome.jsx
@@ -11,31 +11,42 @@ const AdminDashboardHome = () => {
       return res.data;
     },
   });
+
+  console.log(deliveryStatus);
   return (
     <div>
-      <div className="stats shadow">
-        {deliveryStatus.map((stat) => (
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
-              </svg>
+      <div className="bg-white rounded-xl m-5 p-3">
+        <h2 className="text-secondary text-4xl  font-bold ml-3 my-4">
+          Admin Dashboard
+        </h2>
+
+        <div className="overflow-x-auto p-4">
+          <div>
+            <div className="stats shadow">
+              {deliveryStatus.map((stat) => (
+                <div className="stat">
+                  <div className="stat-figure text-secondary">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      className="inline-block h-8 w-8 stroke-current"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className="stat-title text-xl">{stat.status}</div>
+                  <div className="stat-value text-primary">{stat.count}</div>
+                </div>
+              ))}
             </div>
-            <div className="stat-title">{stat._id}</div>
-            <div className="stat-value">{stat.count}</div>
-            <div className="stat-desc">Jan 1st - Feb 1st</div>
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );

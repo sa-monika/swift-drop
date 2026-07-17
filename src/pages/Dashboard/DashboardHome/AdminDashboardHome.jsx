8fc876eeff37aeb62ef7aa3a81dc8d3a8dc1c451
Sum-Monika2004 <13monika04@gmail.com>
2026-07-17 22:27:52 +0600

    piechart added

--- src/pages/Dashboard/DashboardHome/AdminDashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome/AdminDashboardHome.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
-
+import { Legend, Pie, PieChart, Tooltip } from "recharts";
+// import { RechartsDevtools } from "@recharts/devtools";
 const AdminDashboardHome = () => {
   const axiosSecure = useAxiosSecure();
   const { data: deliveryStatus = [] } = useQuery({
@@ -13,6 +14,13 @@ const AdminDashboardHome = () => {
   });
 
   console.log(deliveryStatus);
+
+  const getPieChartData = (data) => {
+    return data.map((item) => {
+      return { name: item.status, value: item.count };
+    });
+  };
+
   return (
     <div>
       <div className="bg-white rounded-xl m-5 p-3">
@@ -22,7 +30,7 @@ const AdminDashboardHome = () => {
 
         <div className="overflow-x-auto p-4">
           <div>
-            <div className="stats shadow">
+            <div className="stats shadow-md">
               {deliveryStatus.map((stat) => (
                 <div className="stat">
                   <div className="stat-figure text-secondary">
@@ -45,6 +53,32 @@ const AdminDashboardHome = () => {
                 </div>
               ))}
             </div>
+          </div>
+          <div className="w-full h-[400px]">
+            <PieChart
+              style={{
+                width: "100%",
+                maxWidth: "500px",
+                maxHeight: "80vh",
+                aspectRatio: 2,
+              }}
+              responsive
+            >
+              <Pie
+                dataKey="value"
+                startAngle={180}
+                endAngle={0}
+                data={getPieChartData(deliveryStatus)}
+                cx="50%"
+                cy="100%"
+                outerRadius="120%"
+                fill="#f3971b"
+                label
+                isAnimationActive={true}
+              />
+              <Legend></Legend>
+              <Tooltip></Tooltip>
+            </PieChart>
           </div>
         </div>
       </div>
